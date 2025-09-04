977c60c589677c95603edabc3e60d90869f9c910
osangwa <osangwa@environment.gov.rw>
2025-09-04 23:50:48 +0200

    partitioning

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,11 +1,20 @@
--- Partition bookings table by YEAR(check_in)
-ALTER TABLE bookings
+-- Create bookings table with partitioning by YEAR(check_in)
+CREATE TABLE bookings (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    property_id BIGINT NOT NULL,
+    check_in DATE NOT NULL,
+    check_out DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (property_id) REFERENCES properties(id) ON DELETE CASCADE
+)
 PARTITION BY RANGE (YEAR(check_in)) (
     PARTITION p2023 VALUES LESS THAN (2024),
     PARTITION p2024 VALUES LESS THAN (2025),
     PARTITION pmax VALUES LESS THAN MAXVALUE
 );
 
--- Test: Fetch bookings in 2024
-SELECT * FROM bookings
+-- Example query to test partitioning
+SELECT *
+FROM bookings
 WHERE YEAR(check_in) = 2024;
