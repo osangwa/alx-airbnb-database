92435927c4f1095336e6e95cf9a2f5f30322b645
osangwa <osangwa@environment.gov.rw>
2025-09-04 23:41:03 +0200

    update

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,9 +1,27 @@
+-- Total bookings per user
+SELECT u.id, u.name, COUNT(b.id) AS total_bookings
+FROM users u
+LEFT JOIN bookings b ON u.id = b.user_id
+GROUP BY u.id, u.name
+ORDER BY u.id;
+
 -- Rank properties by total bookings using ROW_NUMBER()
 SELECT
     p.id,
     p.title,
     COUNT(b.id) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS row_number_rank
+FROM properties p
+LEFT JOIN bookings b ON p.id = b.property_id
+GROUP BY p.id, p.title
+ORDER BY row_number_rank;
+
+-- Rank properties by total bookings using RANK()
+SELECT
+    p.id,
+    p.title,
+    COUNT(b.id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.id) DESC) AS rank
 FROM properties p
 LEFT JOIN bookings b ON p.id = b.property_id
 GROUP BY p.id, p.title
