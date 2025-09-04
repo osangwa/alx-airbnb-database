fe8b01d2527b07eae84f1ee923133c27be5cf35d
osangwa <osangwa@environment.gov.rw>
2025-09-04 23:35:43 +0200

    join querries update

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,9 +4,16 @@ FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
 
 -- LEFT JOIN: Retrieve all properties and their reviews, including properties with no reviews
-SELECT p.id AS property_id, p.title, r.rating, r.comment
+SELECT 
+    p.id AS property_id,
+    p.title,
+    r.id AS review_id,
+    r.user_id AS reviewer_id,
+    r.rating,
+    r.comment
 FROM properties p
 LEFT JOIN reviews r ON p.id = r.property_id;
+
 
 -- FULL OUTER JOIN simulation using UNION (MySQL lacks direct FULL OUTER JOIN)
 SELECT u.id AS user_id, u.name, b.id AS booking_id, b.check_in
